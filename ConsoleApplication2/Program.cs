f8cb0ca47af78d98cc2fbe81576aaa62bb11ba94
焦康 <Administrator@HP201-1>
2017-05-11 10:19:38 +0800

    三元运算符

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -17,15 +17,22 @@ namespace ConsoleApplication2
             //Console.WriteLine(a);//num++为1
             //Console.WriteLine(b);//++num为3
             #endregion
-            
+            #region &&和||
+            //int x = 2, y = 5;
+            //Boolean z = (x > 0) || (y < 8);//||都必须符合条件
+            //Console.WriteLine("z={0}", z);
+            //z = (x == 0) && (y < 8);//&&只有一个符合条件
+            //Console.WriteLine("z={0}",z);
+            //z = !(x == 2);//!不等于
+            //Console.WriteLine("z={0}", z);
+            #endregion
+            #region 三元运算符
+            Console.WriteLine("请输入成绩");
+            int score = int.Parse(Console.ReadLine());
+            string succ = score >= 60 ? "及格" : "不及格";//三元运算符
+            Console.WriteLine(succ);
+            #endregion
 
-            int x = 2, y = 5;
-            Boolean z = (x > 0) || (y < 8);//||都必须符合条件
-            Console.WriteLine("z={0}", z);
-            z = (x == 0) && (y < 8);//&&只有一个符合条件
-            Console.WriteLine("z={0}",z);
-            z = !(x == 2);//!不等于
-            Console.WriteLine("z={0}", z);
         }
     }
 }
