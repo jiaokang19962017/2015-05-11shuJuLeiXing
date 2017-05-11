8411d8c6d84b7b05811d4156721323de88feb138
焦康 <Administrator@HP201-1>
2017-05-11 09:19:13 +0800

    数据类型

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,11 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            double d = 123.456;
+            float f = 1.2f;
+            decimal de = 1.2m;
+            char c = 'w';
+            char c1 = '我';  //char可以存储一个英文字符,一个中文字符
         }
     }
 }
