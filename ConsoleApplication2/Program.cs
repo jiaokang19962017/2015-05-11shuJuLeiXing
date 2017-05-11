77735e448c7d5e7292eecd7af4cce8f48ca301a3
焦康 <Administrator@HP201-1>
2017-05-11 10:12:03 +0800

    &&和||

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,11 +10,22 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            int i = 1;
-            int a = i++;
-            int b = ++i;
-            Console.WriteLine(a);//num++为1
-            Console.WriteLine(b);//++num为3
+            #region i++和++i
+            //int i = 1;
+            //int a = i++;
+            //int b = ++i;
+            //Console.WriteLine(a);//num++为1
+            //Console.WriteLine(b);//++num为3
+            #endregion
+            
+
+            int x = 2, y = 5;
+            Boolean z = (x > 0) || (y < 8);//||都必须符合条件
+            Console.WriteLine("z={0}", z);
+            z = (x == 0) && (y < 8);//&&只有一个符合条件
+            Console.WriteLine("z={0}",z);
+            z = !(x == 2);//!不等于
+            Console.WriteLine("z={0}", z);
         }
     }
 }
